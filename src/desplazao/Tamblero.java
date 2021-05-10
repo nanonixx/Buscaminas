26e9b1263ad60a6be6c56d6248045475b69fc86d
nanonixx <nhid@elpuig.xeill.net>
2021-05-10 19:23:56 +0200

    tablerito estña ahi

---
+++ src/desplazao/Tamblero.java
@@ -0,0 +1,41 @@
+package desplazao;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Tamblero {
+    // 0 -> no está tocado
+    // -1 -> no hay nada de mina
+    // 1 -> hay la Mina
+    int[][] tableroMatriz;
+
+    //dimension del tablero DIMxDIM
+    private int DIMENSION;
+
+    public Tamblero(int DIM) {
+        this.DIMENSION = DIM;
+        this.tableroMatriz = generarTablero();
+    }
+
+    private int[][] generarTablero() {
+        tableroMatriz = new int[DIMENSION][DIMENSION];
+
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                tableroMatriz[i][j] = 0;
+            }
+        }
+        return tableroMatriz;
+    }
+
+
+    public void mostrarTablero() {
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                if (j==0) System.out.print(i + "   ");
+                System.out.print("| " + tableroMatriz[i][j] + " ");
+            }
+            System.out.println("|\n");
+        }
+        System.out.print("      0   1   2   3   4   5   6   7");
+    }
+}
