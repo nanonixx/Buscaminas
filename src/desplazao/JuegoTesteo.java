5441f26829a8dd040b4badb340f210b3dc4f7d2f
Naomi <62404395+nanonixx@users.noreply.github.com>
2021-05-11 11:32:36 +0200

    Lógica juego (sin server) y cambiado cosingas

--- src/desplazao/JuegoTesteo.java
+++ src/desplazao/JuegoTesteo.java
@@ -1,9 +1,51 @@
 package desplazao;
 
+import java.util.Scanner;
+
+//para testear que van las cosas sin depender del server
 public class JuegoTesteo {
+
     public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int minasCazadas = 0;
+    	int player;
+    	boolean valid;
+    	//para repetir turno si no se introducen bien los parámetros
 
         Tablero tablero = new Tablero(8, 10);
-        tablero.mostrarTablero();
+        player = 1;
+        
+        while (minasCazadas != tablero.numMinas) {
+        	System.out.println("TURNO DEL JUGADOR " + player);
+        	
+        	do {
+        		valid = false;
+        		 tablero.mostrarTablero();
+     	        
+     	        System.out.print("\nIntroduce coordenada (fila/columna):  ");
+     	        String tirada = sc.nextLine();
+     	        
+     	        try {
+     		        int hayMina = tablero.recorrer(tirada, player);
+     		        if (hayMina == 1) minasCazadas++;
+     		        if (hayMina != -1) valid = true;
+     		        else System.err.println("Jugada no válida. Casilla ya marcada");
+     	        } catch (Exception e) {
+     	        	System.err.println("Coordenada fuera de rango. Prueba otra vez");
+     	        }
+     	        
+        		
+        	}while(!valid);
+        	
+	       
+	        System.out.println("QUEDAN " + (tablero.numMinas - minasCazadas) + " MINAS POR DESCUBIRIR");
+	        if (player == 1) player = 2;
+	        if (player == 2) player = 1;
+	        
+        }
+        
+        sc.close();
+        
+       
     }
 }
--- src/desplazao/Tablero.java
+++ src/desplazao/Tablero.java
@@ -7,9 +7,10 @@ import java.util.Random;
 import java.io.Serializable;
 
 public class Tablero implements Serializable {
-    // 0 -> no estÃ¡ tocado
-    // -1 -> no hay nada de mina
-    // 1 -> hay la Mina
+    // ? -> no estÃ¡ tocado
+    // -  -> No hay la mina
+    // 1 -> hay la Mina de P1
+	//2 -> hay mina de P2
     Casilla[][] tableroMatriz;
     int numMinas;
 
@@ -29,7 +30,7 @@ public class Tablero implements Serializable {
 
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
-                tableroMatriz[i][j] = new Casilla("?");
+                tableroMatriz[i][j] = new Casilla(i+""+j);
             }
         }
 
@@ -56,22 +57,33 @@ public class Tablero implements Serializable {
                 if (j==0) System.out.print(i + "   ");
                 System.out.print("| " + tableroMatriz[i][j].marca + " ");
 //                System.out.print("| " + tableroMatriz[i][j].mina + " ");
+                //activar el comment de arriba para ver las minas
             }
             System.out.println("|\n");
         }
-        System.out.print("      0   1   2   3   4   5   6   7");
+        System.out.print("       0    1    2    3    4    5    6    7");
     }
-
-    public boolean completado(){
-        boolean finish = true;
-
-        for (int i = 0; i < DIMENSION; i++) {
-            for (int j = 0; j < DIMENSION; j++) {
-                //TODO: hacerlo bien luego esto:
-                if (tableroMatriz[i][j].marca == "?") finish = false;
-            }
+    
+    //este método pone las marcas en el tablero y devuelve si ha encontrado mina o no
+    public int recorrer(String coor, int player) {
+    	//int player es el num. del jugador del que está en turno
+    	int fila = Integer.parseInt(coor.substring(0, 1));
+    	int columna = Integer.parseInt(coor.substring(1));
+    	    	
+    	if (tableroMatriz[fila][columna].marca.length()!=2) {
+    		//si la casilla ya ha sido descubierta
+    		return -1;
+    		
+    	}    	
+    	else if (tableroMatriz[fila][columna].mina) {
+        	tableroMatriz[fila][columna].marca = String.valueOf(player+" ");
+        	return 1; //mina encontrada
         }
-        return finish;
+        else {
+        	tableroMatriz[fila][columna].marca = "-";
+        	return 0; //mina no encontrada
+        }
+    	
     }
 
 
